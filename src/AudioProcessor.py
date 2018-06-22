ce22a2cf71792bfc91ee94db4589d6d3dddad085
hitesh.bagchi <hiteshbagchi@gmail.com>
2018-06-22 18:22:39 +0530

    Precision, Recall, F1-score added with plot

--- src/AudioProcessor.py
+++ src/AudioProcessor.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'figure.max_open_warning': 0})
 from matplotlib.pyplot import specgram
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 from IPython.display import SVG
 
@@ -17,6 +18,7 @@ import librosa, librosa.display
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
+from keras.callbacks import Callback
 from keras.utils.vis_utils import model_to_dot
 from keras.utils import np_utils
 
@@ -115,6 +117,35 @@ def dump_features(features, features_filename):
     features_df.to_pickle(features_filename)
 
 # %%
+class Metrics(Callback):
+
+    def on_train_begin(self, logs={}):
+        self.val_f1s = []
+        self.val_recalls = []
+        self.val_precisions = []
+        self.val_epochs = []
+
+    def on_epoch_end(self, epoch, logs={}):
+
+        self.val_epochs.append(epoch)
+
+        val_predict = (np.asarray(self.model.predict(self.validation_data[0]))).round()
+        val_targ = self.validation_data[1]
+
+        _val_f1 = f1_score(val_targ, val_predict, average="micro")
+        _val_recall = recall_score(val_targ, val_predict, average="micro")
+        _val_precision = precision_score(val_targ, val_predict, average="micro")
+
+        self.val_f1s.append(_val_f1)
+        self.val_recalls.append(_val_recall)
+        self.val_precisions.append(_val_precision)
+
+        print(" — val_f1: %f — val_precision: %f — val_recall: %f"
+              %(_val_f1, _val_precision, _val_recall), '\n')
+
+metrics = Metrics()
+
+# %%
 features_train_file = Path("./features_train.pkl")
 
 if not features_train_file.is_file():
@@ -164,7 +195,15 @@ X = np.vstack(X)
 
 X.shape, y.shape
 
-model.fit(X, y, batch_size=32, epochs=50, validation_split=0.20)
+model.fit(X, y, batch_size=32, epochs=50, validation_split=0.20, callbacks=[metrics])
+
+# %%
+# Plot the cost against epoch
+f1s_plot, = plt.plot(metrics.val_epochs, metrics.val_f1s, 'r+')
+precisions_plot, = plt.plot(metrics.val_epochs, metrics.val_precisions, 'b*')
+recalls_plot, = plt.plot(metrics.val_epochs, metrics.val_recalls, 'g^')
+
+plt.legend([f1s_plot, precisions_plot, recalls_plot], ["F1-Score", "Precision", "Recall" ])
 
 # %%
 

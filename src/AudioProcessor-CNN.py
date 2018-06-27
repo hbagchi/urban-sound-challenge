6828ef10553081cd6151a59ebd41440561ad72d3
hitesh.bagchi <hiteshbagchi@gmail.com>
2018-06-27 19:00:46 +0530

    Extract Features for CNN updated

--- src/AudioProcessor-CNN.py
+++ src/AudioProcessor-CNN.py
@@ -26,16 +26,18 @@ def windows(data, window_size):
 #--------------------------------------------------------------------------------------------------
 def extract_features(data_dir, row, bands = 60, frames = 41, file_ext="*.wav"):
 
+    import ipdb
+    ipdb.set_trace()
+
     window_size = 512 * (frames - 1)
     log_specgrams = []
-    labels = []
 
     file_name = os.path.join(os.path.abspath('./data/'), data_dir, str(row.ID) + '.wav')
 
     # handle exception to check for corrupted or invalid file
     try:
         X, sample_rate = librosa.load(file_name)
-        label = row.Class
+
         for (start, end) in windows(X, window_size):
             if(len(X[start:end]) == window_size):
                 signal = X[start:end]
@@ -43,7 +45,6 @@ def extract_features(data_dir, row, bands = 60, frames = 41, file_ext="*.wav"):
                 logspec = librosa.amplitude_to_db(melspec)
                 logspec = logspec.T.flatten()[:, np.newaxis].T
                 log_specgrams.append(logspec)
-                labels.append(label)
 
         log_specgrams = np.asarray(log_specgrams).reshape(len(log_specgrams),bands,frames, 1)
         features = np.concatenate((log_specgrams, np.zeros(np.shape(log_specgrams))), axis = 3)
@@ -55,15 +56,16 @@ def extract_features(data_dir, row, bands = 60, frames = 41, file_ext="*.wav"):
         print(file_name, e)
         return None
 
-    return np.array(features), np.array(labels)
+    return features
 
 # %%
 #--------------------------------------------------------------------------------------------------
 # Load training data and extract features
 #--------------------------------------------------------------------------------------------------
-DATA_DIR = 'trainShort'
+DATA_DIR = 'trainMini'
 
 train = pd.read_csv(os.path.join(os.path.abspath('./data/'), DATA_DIR + '.csv'))
 
 extract_features_train = partial(extract_features, DATA_DIR)
-features, labels = train.apply(extract_features_train, axis=1)
+features = train.apply(extract_features_train, axis=1)
+
